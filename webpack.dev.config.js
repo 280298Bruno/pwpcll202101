80b7c66a4173c77a00d1f260b1ff2227415337d5
280298Bruno <gabetcastro1978@gmail.com>
2021-10-19 23:04:38 -0500

    Integrando Babel

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -15,7 +15,44 @@ module.exports = {
     },
     devServer: {
         static: path.join(__dirname, 'public'),
-        port: 8080,
+        port: 8085,
         host: 'localhost'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_componentes)/,
+                use: [
+                    {
+                        loader:'babel-loader',
+                        options: {
+                            presets: [
+                                [
+                                    '@babel/preset-env',
+                                    {
+                                        'modules': false,
+                                        'useBuiltIns':'usage',
+                                        'targets': {"chrome": "80"},
+                                        'corejs': 3
+                                    }
+                                ]
+                            ],
+                            "plugins": [
+                                [
+                                    "module-resolver",
+                                    {
+                                        "root": ["./"],
+                                        "alias": {
+                                            "@client" : "./client",
+                                        }
+                                    }
+                                ]
+                            ]
+                        }
+                    }
+                ]
+            }
+        ]
     }
 }
